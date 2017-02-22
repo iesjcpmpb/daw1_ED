c8f880398b9ec053d1e5ab205153d6e66164c35c
Pasqual <iesjc.pmpb@gmail.com>
2017-02-22 20:40:57 +0100

    Primer commit hola mundo

--- holamundo.java
+++ holamundo.java
@@ -4,5 +4,6 @@ class HolaMundoApp {
 	public static void main( String args[] ) {
 		System.out.println( "Hola Mundo!" );
 		System.out.println( "Hola Git!" );
+		System.out.println( "Hola Git2!" );
 	}
 }
